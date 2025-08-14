4df8dd7aad1d3ecd21b40632f10f704121ec9020
jeonDev <jjhwhdgus0727@gmail.com>
2025-08-14 22:17:54 +0900

    Initial commit

---
+++ src/main/java/com/chat/main/TcpChatApplication.java
@@ -0,0 +1,13 @@
+package com.chat.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcpChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcpChatApplication.class, args);
+    }
+
+}
