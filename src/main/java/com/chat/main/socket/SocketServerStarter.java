1509d7156d6461ef4dc80bed8a0f00c45a8d5531
jeonDev <jjhwhdgus0727@gmail.com>
2025-09-01 21:19:18 +0900

    fix ( netty ) : Netty 실행 쓰레드 추가

--- src/main/java/com/chat/main/socket/SocketServerStarter.java
+++ src/main/java/com/chat/main/socket/SocketServerStarter.java
@@ -2,6 +2,7 @@ package com.chat.main.socket;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -14,16 +15,28 @@ public class SocketServerStarter {
     private final ServerBootstrap serverBootStrap;
 
     public SocketServerStarter(@Value("${socket.chat.server.port}") int port,
-                               ServerBootstrap serverBootStrap) throws InterruptedException {
+                               ServerBootstrap serverBootStrap
+    ) {
         this.port = port;
         this.serverBootStrap = serverBootStrap;
-        this.start();
     }
 
-    public void start() throws InterruptedException {
+    private void start() throws InterruptedException {
         log.info("[Netty] Server Start");
         ChannelFuture future = serverBootStrap.bind(port).sync();
         future.channel().closeFuture().sync();
+    }
+
+    @PostConstruct
+    public void startNettyServer() {
+        new Thread(() -> {
+            try {
+                this.start();
+            } catch (InterruptedException e) {
+                log.error("Netty Server 실행 중 오류 발생 : {}", e.getMessage(), e);
+                Thread.currentThread().interrupt();
+            }
+        }, "Netty-Tcp").start();
     }
 
 }
